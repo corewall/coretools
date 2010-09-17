66d3d516e02319abccac3eddc4f870e7cb9bef55
Josh Reed <jareed@andrill.org>
2010-09-17 15:14:36 -0500

    Added a StandardFormats module - registers TSV formats for Section and Image - registers CSV formats for Section and Image - registers Excel formats for Section and Image

---
+++ core/src/main/java/org/corewall/data/formats/StandardFormats.java
@@ -0,0 +1,37 @@
+package org.corewall.data.formats;
+
+import org.corewall.data.Format;
+import org.corewall.data.Model;
+import org.corewall.geology.models.Image;
+import org.corewall.geology.models.Section;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.Multibinder;
+
+/**
+ * Registers the standard {@link Format}s.
+ * 
+ * @author Josh Reed (jareed@andrill.org)
+ */
+public class StandardFormats extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		Multibinder<Format> formats = Multibinder.newSetBinder(binder(), Format.class);
+
+		// tab-separated
+		formats.addBinding().toInstance(new CSVFormat<Model>("tsv:raw", '\t', null));
+		formats.addBinding().toInstance(new CSVFormat<Section>("tsv:Section", '\t', Section.factory()));
+		formats.addBinding().toInstance(new CSVFormat<Image>("tsv:Image", '\t', Image.factory()));
+
+		// comma-separated
+		formats.addBinding().toInstance(new CSVFormat<Model>("csv:raw", ',', null));
+		formats.addBinding().toInstance(new CSVFormat<Section>("csv:Section", ',', Section.factory()));
+		formats.addBinding().toInstance(new CSVFormat<Image>("csv:Image", ',', Image.factory()));
+
+		// excel
+		formats.addBinding().toInstance(new ExcelFormat<Model>("excel:raw", 0, null));
+		formats.addBinding().toInstance(new ExcelFormat<Section>("excel:Section", "Sections", Section.factory()));
+		formats.addBinding().toInstance(new ExcelFormat<Image>("excel:Image", "Images", Image.factory()));
+	}
+}
