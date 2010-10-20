eb2767a99b267e21036db4a3538337cc4bc167e3
Josh Reed <jareed@andrill.org>
2010-10-20 14:43:06 -0500

    Implemented sorting on the table

--- apps/DataManager/src/main/java/org/corewall/ui/data/ProjectPanel.java
+++ apps/DataManager/src/main/java/org/corewall/ui/data/ProjectPanel.java
@@ -3,6 +3,8 @@ package org.corewall.ui.data;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.net.URL;
+import java.util.Comparator;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,11 +16,13 @@ import org.corewall.data.Project;
 import org.corewall.data.Project.ManifestEntry;
 
 import ca.odell.glazedlists.BasicEventList;
-import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.SortedList;
 import ca.odell.glazedlists.gui.TableFormat;
 import ca.odell.glazedlists.swing.EventTableModel;
 
 import com.explodingpixels.macwidgets.MacWidgetFactory;
+import com.explodingpixels.widgets.TableUtils;
+import com.explodingpixels.widgets.TableUtils.SortDirection;
 import com.google.inject.internal.Nullable;
 
 /**
@@ -27,10 +31,41 @@ import com.google.inject.internal.Nullable;
  * @author Josh Reed (jareed@andrill.org)
  */
 public class ProjectPanel extends JPanel {
+	private static class ManifestEntryComparator implements Comparator<ManifestEntry> {
+		private final boolean ascending;
+		private final String field;
+
+		private ManifestEntryComparator(final String field, final boolean ascending) {
+			this.field = field;
+			this.ascending = ascending;
+		}
+
+		@Override
+		public int compare(final ManifestEntry o1, final ManifestEntry o2) {
+			String f1, f2;
+			if ("name".equals(field)) {
+				f1 = o1.getName();
+				f2 = o2.getName();
+			} else if ("type".equals(field)) {
+				f1 = o1.getType();
+				f2 = o2.getType();
+			} else if ("format".equals(field)) {
+				f1 = o1.getFormat();
+				f2 = o2.getFormat();
+			} else {
+				URL u1 = o1.getPath();
+				f1 = (u1 == null ? "" : u1.toExternalForm());
+				URL u2 = o2.getPath();
+				f2 = (u2 == null ? "" : u2.toExternalForm());
+			}
+			return (ascending ? 1 : -1) * f1.compareTo(f2);
+		}
+	}
+
 	private static final long serialVersionUID = 1L;
 
 	protected JLabel label;
-	protected EventList<Project.ManifestEntry> manifest;
+	protected SortedList<ManifestEntry> manifest;
 	protected Project project = null;
 	protected JTable table;
 
@@ -48,9 +83,10 @@ public class ProjectPanel extends JPanel {
 		add(label, BorderLayout.NORTH);
 
 		// our list of manifest entries
-		manifest = new BasicEventList<Project.ManifestEntry>();
-		EventTableModel<Project.ManifestEntry> model = new EventTableModel<Project.ManifestEntry>(manifest,
-				new TableFormat<Project.ManifestEntry>() {
+		manifest = new SortedList<ManifestEntry>(new BasicEventList<ManifestEntry>(), new ManifestEntryComparator(
+				"type", true));
+		EventTableModel<ManifestEntry> model = new EventTableModel<ManifestEntry>(manifest,
+				new TableFormat<ManifestEntry>() {
 					@Override
 					public int getColumnCount() {
 						return 4;
@@ -90,7 +126,29 @@ public class ProjectPanel extends JPanel {
 				});
 		table = MacWidgetFactory.createITunesTable(model);
 		table.setFillsViewportHeight(true);
-		table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+		TableUtils.SortDelegate sortDelegate = new TableUtils.SortDelegate() {
+			public void sort(final int column, final TableUtils.SortDirection sortDirection) {
+				switch (column) {
+					case 0:
+						manifest.setComparator(new ManifestEntryComparator("name",
+								sortDirection == SortDirection.ASCENDING));
+						break;
+					case 1:
+						manifest.setComparator(new ManifestEntryComparator("type",
+								sortDirection == SortDirection.ASCENDING));
+						break;
+					case 2:
+						manifest.setComparator(new ManifestEntryComparator("format",
+								sortDirection == SortDirection.ASCENDING));
+						break;
+					case 3:
+						manifest.setComparator(new ManifestEntryComparator("path",
+								sortDirection == SortDirection.ASCENDING));
+						break;
+				}
+			}
+		};
+		TableUtils.makeSortable(table, sortDelegate);
 		JScrollPane scrollPane = new JScrollPane(table);
 		add(scrollPane, BorderLayout.CENTER);
 	}
